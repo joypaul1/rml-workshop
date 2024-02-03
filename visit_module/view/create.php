a22fd851aed811bf980481c28aa76fc859b03b0e
joypaul1 <jyla5102555@gmail.com>
2024-02-03 13:01:02 +0600

    create

--- visit_module/view/create.php
+++ visit_module/view/create.php
@@ -1,7 +1,9 @@
 <?php
+$dynamic_link_css[] = '../../assets/plugins/select2/css/select2.min.css';
 $dynamic_link_css[] = '../../assets/plugins/datetimepicker/css/classic.css';
 $dynamic_link_css[] = '../../assets/plugins/datetimepicker/css/classic.date.css';
-
+$dynamic_link_css[] = '../../assets/plugins/select2/css/select2-bootstrap4.css';
+$dynamic_link_js[]  = '../../assets/plugins/select2/js/select2.min.js';
 $dynamic_link_js[]  = '../../assets/plugins/datetimepicker/js/picker.js';
 $dynamic_link_js[]  = '../../assets/plugins/datetimepicker/js/picker.date.js';
 $dynamic_link_js[]  = '../../assets/plugins/bootstrap-material-datetimepicker/js/moment.min.js';
@@ -9,7 +11,7 @@ $dynamic_link_js[]  = '../../assets/plugins/bootstrap-material-datetimepicker/js
 
 include_once('../../_helper/2step_com_conn.php');
 // if( $_SESSION['USER_SFCM_INFO']['USER_TYPE'] == "HOD" ||  $_SESSION['USER_SFCM_INFO']['USER_TYPE']== 'COORDINATOR'){
-        
+
 // }
 
 ?>
@@ -17,29 +19,66 @@ include_once('../../_helper/2step_com_conn.php');
 <!--start page wrapper -->
 <div class="page-wrapper">
     <div class="page-content">
+
         <div class="card rounded-4">
             <?php
             $headerType   = 'List';
             $leftSideName = 'Visit Create or Assign For Me';
             include('../../_includes/com_header.php');
             ?>
+            <div class="cards rounded-4">
+                <div class="card-body">
+
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8'); ?>" method="POST">
+                        <div class="row justify-content-center align-items-center text-center">
+                            <div class="col-3">
+                                <label class="form-label">Retailer Disctrict:</label>
+                                <select name="disctrictID" class="form-control 
+                                text-center single-select">
+                                    <option value="<?php echo null ?>" hidden><- Select Disctrict -></option>
+                                    <?php
+                                    $strSQL = oci_parse($objConnect, "SELECT ID,NAME FROM DISTRICT WHERE STATUS =1");
+                                    oci_execute($strSQL);
+
+                                    while ($row = oci_fetch_assoc($strSQL)) {
+                                    ?>
+                                        <option value="<?php echo $row['ID'] ?>" <?php echo isset($_POST['disctrictID']) && $_POST['disctrictID'] == $row['ID'] ? 'Selected' : '' ?>>
+                                            <?php echo $row['NAME'] ?>
+                                        </option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="col-sm-3">
+                                <label>Retailer Name/Mobile : </label>
+                                <input class="form-control" name="USER_NAME_MOBILE" type="text" value='<?php echo isset($_POST['USER_NAME_MOBILE']) ? $_POST['USER_NAME_MOBILE'] : ''; ?>' />
+                            </div>
+                            <div class="col-sm-4 d-flex gap-2">
+                                <button type="submit" class="form-control btn btn-sm btn-gradient-primary mt-4">Search Data<i class='bx bx-file-find'></i></button>
+                                <a href="<?php echo $sfcmBasePath  ?>/visit_module/view/create.php" class="form-control btn btn-sm btn-gradient-info mt-4">Reset Data<i class='bx bx-file'></i></a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
             <div class="card-body">
-                <div class=" card card-body col-sm-12 col-md-9  col-xl-9 mx-auto p-4 border rounded">
+                <div class=" card card-body col-sm-12 col-md-12  col-xl-12 mx-auto p-4 border rounded">
                     <form method="POST" action="<?php echo ($sfcmBasePath . '/visit_module/action/self_panel.php') ?>">
                         <div class="row justify-content-center align-items-center ">
                             <input type="hidden" name="actionType" value="create">
 
-                            <div class="col-5 text-center form-group mb-3">
-                                <label>Select Date: </label>
+                            <div class="col-3">
+                                <label class="form-label">Select Date: </label>
                                 <div class="input-group">
                                     <input required="" class="form-control datepicker" name="date" type="text" value='<?php echo isset($_POST['date']) ? $_POST['date'] : date('d-m-Y'); ?>' />
                                 </div>
-
                             </div>
 
-                            <div class="col-5 text-center form-group mb-3">
-                                <label>Select Visit Type : </label>
-                                <select id="inputState" required name="visit_type" class="form-select text-center">
+                            <div class="col-3">
+                                <label class="form-label">Select Visit Type : </label>
+                                <select id="inputState" required name="visit_type" class="form-control 
+                                text-center single-select">
                                     <option hidden value="<?php echo null ?>"> <- Select Type Data -></option>
                                     <?php
 
@@ -55,32 +94,47 @@ include_once('../../_helper/2step_com_conn.php');
                                 </select>
 
                             </div>
+
+                            <div class="col-3">
+                                <label>MOBILE : </label>
+                                <input class="form-control" onkeypress='return event.charCode >= 48 && event.charCode <= 57' name="USER_MOBILE" type="text" value='<?php echo isset($_POST['USER_MOBILE']) ? $_POST['USER_MOBILE'] : ''; ?>' />
+                            </div>
                             <div class="shadow-sm p-2 mb-2 bg-white rounded text-center">
 
-                                <strong class="text-success rounded fs-6"> Select Your Retailer <i class='bx bx-select-multiple'></i></strong>
+                                <strong class="text-primary rounded fs-6"> Please Select Your Retailer <i class='bx bx-select-multiple'></i></strong>
 
                             </div>
                             <div class="form-group mb-3">
 
-
                                 <?php
-
                                 $USER_BRANDS = $_SESSION['USER_SFCM_INFO']['USER_BRANDS'] ? $_SESSION['USER_SFCM_INFO']['USER_BRANDS'] : 0;
-                                    // echo $USER_BRANDS;
                                 $query = "SELECT 
-                                        UP.ID, 
-                                        UP.USER_NAME, 
-                                        LISTAGG(UBS.PRODUCT_BRAND_ID, ', ') WITHIN GROUP (ORDER BY UBS.PRODUCT_BRAND_ID) AS USER_BRANDS
+                                            UP.ID, 
+                                            UP.USER_NAME, 
+                                            UP.USER_MOBILE
                                         FROM 
                                             USER_PROFILE UP
                                         LEFT JOIN 
                                             USER_BRAND_SETUP UBS ON UBS.USER_PROFILE_ID = UP.ID
                                         WHERE 
                                             UBS.PRODUCT_BRAND_ID IN ($USER_BRANDS)
-                                            AND UBS.STATUS = 1
-                                            AND UP.USER_TYPE_ID = 4
-                                        GROUP BY 
-                                            UP.ID, UP.USER_NAME";
+                                            AND UBS.STATUS = 1  
+                                            AND UP.USER_TYPE_ID = 4";
+                                if (isset($_POST['USER_NAME_MOBILE'])) {
+                                    if (!empty($_POST['USER_NAME_MOBILE'])) {
+                                        $searchTerm = str_replace(" ", "_", trim($_POST['USER_NAME_MOBILE']));
+                                        $query .= " AND (UP.USER_NAME LIKE '%" . $searchTerm . "%' OR UP.USER_MOBILE LIKE '%" . $searchTerm . "%')";
+                                    }
+                                }
+                                if (isset($_POST['disctrictID'])) {
+                                    if (!empty($_POST['disctrictID'])) {
+                                        $searchTerm = trim($_POST['disctrictID']);
+                                        $query .= " AND UP.DISTRICT_ID = " . $searchTerm;
+                                    }
+                                }
+                                $query .= " GROUP BY 
+                                UP.ID, UP.USER_NAME, UP.USER_MOBILE";
+
                                 $strSQL = oci_parse($objConnect, $query);
                                 // echo $query;
                                 oci_execute($strSQL);
@@ -88,14 +142,15 @@ include_once('../../_helper/2step_com_conn.php');
                                 while ($row = oci_fetch_assoc($strSQL)) {
 
                                 ?>
-                                    <span class="row">
-                                        <div class="col-6 form-check ">
+                                    <span class="row justify-content-center">
+
+                                        <div class="col-4 form-check ">
+                                            <i class='bx bxs-chevrons-right text-success'></i>
                                             <input class="form-check-input" name="user_id[<?php echo $row['ID'] ?>]" type="checkbox" value="<?php echo $row['ID'] ?>" id="flexCheckChecked_<?php echo $row['ID'] ?>">
                                             <label class="form-check-label" for="flexCheckChecked_<?php echo $row['ID'] ?>"><?php echo $row['USER_NAME'] ?></label>
                                         </div>
-                                        <div class="col-6 form-check mb-2">
+                                        <div class="col-4 form-check mb-2">
                                             <input type="text" name="user_remarks[<?php echo $row['ID'] ?>]" placeholder="Any Remarks?" class="form-control" id="inputFirstName">
-                                            <!-- <label class="form-check-label" for="flexCheckChecked_<?php echo $row['ID'] ?>"><?php echo $row['USER_NAME'] ?></label> -->
                                         </div>
                                     </span>
                                 <?php
@@ -127,6 +182,13 @@ include_once('../../_includes/footer_info.php');
 include_once('../../_includes/footer.php');
 ?>
 <script>
+    //select 2
+    $('.single-select').select2({
+        theme: 'bootstrap4',
+        width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+        placeholder: $(this).data('placeholder'),
+        allowClear: Boolean($(this).data('allow-clear')),
+    });
     // Get the current date
     var today = new Date();
 
